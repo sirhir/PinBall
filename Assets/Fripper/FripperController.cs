f19265025f5c95c5de62dde78eb0cda4e2d8bf61
sirhir <76894370+sirhir@users.noreply.github.com>
2021-01-26 11:41:48 +0900

    Initial commit

---
+++ Assets/Fripper/FripperController.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FripperController : MonoBehaviour
+{
+    private HingeJoint myHingeJoint;
+    private float defaultAngle = 20;
+    private float flickAngle = -20;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.myHingeJoint = GetComponent<HingeJoint>();
+        SetAngle(this.defaultAngle);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //キーボード上での入力チェック
+        bool leftButton = false;
+        bool rightButton = false;
+        if ( Input.GetKey(KeyCode.LeftArrow)
+            || Input.GetKey(KeyCode.A)
+            || Input.GetKey(KeyCode.S)
+        ){
+            leftButton = true;
+        }
+        if ( Input.GetKey(KeyCode.RightArrow)
+            || Input.GetKey(KeyCode.D)
+            || Input.GetKey(KeyCode.S)
+        ){
+            rightButton = true;
+        }
+
+        //スマホ等での入力チェック
+        bool leftTouch = false;
+        bool rightTouch = false;
+        foreach ( Touch touch in Input.touches )
+        {
+            if ( touch.position.x < Screen.width/2 )
+            {
+                leftTouch = true;
+            }else{
+                rightTouch = true;
+            }
+        }
+
+
+        if (tag == "LeftFripperTag")
+        {
+            if ( leftButton || leftTouch )
+            {
+                SetAngle (this.flickAngle);
+            }else{
+                SetAngle (this.defaultAngle);
+            }
+        }
+
+        if (tag == "RightFripperTag"){
+            if ( rightButton|| rightTouch )
+            {
+                SetAngle (this.flickAngle);
+            }else{
+                SetAngle (this.defaultAngle);
+            }
+        }
+
+    }
+
+    //フリッパーの傾きを設定
+    public void SetAngle (float angle)
+    {
+        JointSpring jointSpr = this.myHingeJoint.spring;
+        jointSpr.targetPosition = angle;
+        this.myHingeJoint.spring = jointSpr;
+    }
+}
---
+++ Assets/ScoreCalculator.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreCalculator : MonoBehaviour
+{
+    private GameObject scoreText;
+
+    private int currentScore;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.scoreText = GameObject.Find("ScoreText");
+        this.currentScore = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.scoreText.GetComponent<Text>().text = currentScore.ToString();
+    }
+
+
+    void OnCollisionEnter(Collision other)
+    {
+        int getScore = 0;//test
+        // タグによって得られる点数を変える
+        if (other.gameObject.tag == "SmallStarTag")
+        {
+            getScore = 1;
+        }
+        else if (other.gameObject.tag == "LargeStarTag")
+        {
+            getScore = 20;
+        }
+        else if(other.gameObject.tag == "SmallCloudTag")
+        {
+            getScore = 5;
+        }
+        else if(other.gameObject.tag == "LargeCloudTag")
+        {
+            getScore = 3;
+        }
+
+        this.currentScore += getScore;
+    }
+}
